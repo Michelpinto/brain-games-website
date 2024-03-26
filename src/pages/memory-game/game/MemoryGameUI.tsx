277da96a0e08a99bf28d4a8c4eccfd63279cf0d4
michelpinto <michabbe9@gmail.com>
2024-03-26 13:03:59 +0200

    latest updates

--- src/pages/memory-game/game/MemoryGameUI.tsx
+++ src/pages/memory-game/game/MemoryGameUI.tsx
@@ -1,20 +1,43 @@
-import React from 'react';
+import React, { Dispatch, SetStateAction } from 'react';
 import { IoChevronBackOutline } from 'react-icons/io5';
 import { Link } from 'react-router-dom';
 
-import { Button, CardWrapper, Div, FrontFace, Main } from './styles';
+import {
+  Button,
+  CardWrapper,
+  Container,
+  Div,
+  FrontFace,
+  InsideContainer,
+  Main,
+  Overlay,
+  Text,
+} from './styles';
 import { Card } from './MemoryGame';
+import Modal from '../../../components/modal/Modal';
 
 interface IProps {
   cards: Card[];
   handleClick: (index: number) => void;
   handleReset: () => void;
+  moves: number;
+  misses: number;
+  message: string;
+  showModal: boolean;
+  setShowModal: Dispatch<SetStateAction<boolean>>;
+  revealCards: boolean;
 }
 
 const MemoryGameUI: React.FC<IProps> = ({
   cards,
   handleClick,
   handleReset,
+  moves,
+  misses,
+  message,
+  showModal,
+  setShowModal,
+  revealCards,
 }) => {
   let columns;
 
@@ -37,28 +60,63 @@ const MemoryGameUI: React.FC<IProps> = ({
   }
 
   return (
-    <Main>
-      <Link to='/levels'>
-        <IoChevronBackOutline />
-      </Link>
+    <>
+      <Main>
+        <Link to='/levels'>
+          <IoChevronBackOutline />
+        </Link>
 
-      <section>
-        <h1>Memory card game - {cards.length} cards</h1>
-        <Div columns={columns}>
-          {cards.map((card, index) => (
-            <CardWrapper
-              className={card.matched ? 'matched' : ''}
-              key={index}
-              onClick={() => handleClick(index)}
-            >
-              <FrontFace>{card.flipped ? card.content : '?'}</FrontFace>
-            </CardWrapper>
-          ))}
-        </Div>
+        <section>
+          <h1>Memory card game - {cards.length} cards</h1>
+          <Div columns={columns}>
+            {cards.map((card, index) => (
+              <CardWrapper
+                className={card.matched ? 'matched' : ''}
+                key={index}
+                onClick={() => handleClick(index)}
+              >
+                {revealCards ? (
+                  <FrontFace>{card.content}</FrontFace>
+                ) : (
+                  <FrontFace>{card.flipped ? card.content : '?'}</FrontFace>
+                )}
+              </CardWrapper>
+            ))}
+          </Div>
 
-        <Button onClick={() => handleReset()}>Repeat game</Button>
-      </section>
-    </Main>
+          <Container>
+            <Text>
+              <span>{moves}</span> Moves
+            </Text>
+            <Text>
+              <span>{misses}</span> Misses
+            </Text>
+            <Button onClick={() => handleReset()}>Repeat game</Button>
+          </Container>
+        </section>
+      </Main>
+
+      {showModal && (
+        <Modal>
+          <InsideContainer>
+            {message}
+            {Math.abs(moves - misses) <= 5 ? (
+              <Button
+                onClick={() => {
+                  handleReset();
+                  setShowModal(false);
+                }}
+              >
+                Repeat game
+              </Button>
+            ) : (
+              <Button>Next level</Button>
+            )}
+          </InsideContainer>
+        </Modal>
+      )}
+      {showModal && <Overlay />}
+    </>
   );
 };
 
