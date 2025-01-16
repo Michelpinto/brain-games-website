854233ec2dd6ad3000759dd4d64812f0ae9a532e
michelpinto <michabbe9@gmail.com>
2025-01-16 14:40:31 +0000

    styles

--- src/pages/word-search/styles.ts
+++ src/pages/word-search/styles.ts
@@ -2,4 +2,33 @@ import styled from 'styled-components';
 
 export const Main = styled.main`
   text-align: center;
+  display: flex;
+  flex-direction: column;
+  gap: 10rem;
+
+  & nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    & svg {
+      font-size: 3rem;
+    }
+  }
+`;
+
+export const Section = styled.section`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: 1fr;
+  grid-column-gap: 4rem;
+`;
+
+export const Div = styled.div`
+  border-radius: 0.5rem;
+  background-color: inherit;
+  border: 1px solid #f5f5f5;
+  color: #f5f5f5;
+  padding: 1.5rem;
+  cursor: pointer;
 `;
