f34bdbdb94fbb0d0f507a9995d579bf20db5a3e5
michelpinto <michabbe9@gmail.com>
2025-01-16 14:40:18 +0000

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import MemoryGame from './pages/memory-game/game/MemoryGame';
 import WordSearch from './pages/word-search/WordSearch';
 import Page from './pages/Page';
 import Levels from './pages/memory-game/levels/Levels';
+import Game from './pages/word-search/game/Game';
 
 function App() {
   return (
@@ -11,8 +12,8 @@ function App() {
         <Route path='/' element={<Page />} />
         <Route path='/levels' element={<Levels />} />
         <Route path='/levels/*' element={<MemoryGame />} />
-        {/* <Route path='memoryGame' element={<MemoryGame />} /> */}
         <Route path='/WordSearch' element={<WordSearch />} />
+        <Route path='/WordSearch/:topic' element={<Game />} />
       </Routes>
     </>
   );
